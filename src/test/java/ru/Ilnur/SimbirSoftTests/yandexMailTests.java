ac215da42e1176311707ae4ea62f30cf81b14b75
Umar222 <rabotatest1992@mail.ru>
2021-04-24 15:13:34 +0300

    1) Создал метод метод перехода на следующую вкладку 2) Добавил проверку названия темы assertEquals(title.getText(), "Simbirsoft theme"); хотя мне кажется что подобные проверки не уместны для тестов т.к яндекс присылают рекламму, название темы может меняться и при прохождении ассерта тесты всегда будут падать

--- src/test/java/ru/Ilnur/SimbirSoftTests/yandexMailTests.java
+++ src/test/java/ru/Ilnur/SimbirSoftTests/yandexMailTests.java
@@ -4,52 +4,73 @@ import jdk.jfr.Description;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import ru.Ilnur.Pages.mailPage;
+import ru.Ilnur.Pages.MailPage;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-public class yandexMailTests {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+public class YandexMailTests {
 
     public static WebDriver driver = new ChromeDriver();
-    static mailPage mailPage;
-    int count;
-
+    static MailPage mailPage;
+    int n;
 
     @BeforeAll
     public static void setUp() {
-        mailPage = new mailPage(driver);
+        mailPage = new MailPage(driver);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://yandex.ru/");
         mailPage.clickEnter();
         mailPage.openMail("testovumar@yandex.ru", "qwertY116");
         mailPage.openMailWindow();
-
-
     }
 
 
-//    @Description("Проверка отображения писем на тему Simbirsoft theme")
+    @Description("Проверка отображения писем на тему Simbirsoft theme")
     @Test
     public void checkEnterMessage() {
-        ArrayList<String> tabs2 = new ArrayList<String>(driver.getWindowHandles());
-        driver.switchTo().window(tabs2.get(1));
-        List<WebElement> elements = driver.findElements(By.xpath("//span[@class=\"mail-MessageSnippet-Item mail-MessageSnippet-Item_subject\"]"));
-        for (WebElement title : elements) {
+        mailPage.switchNewPage(1);
+        for (WebElement title : mailPage.messageTheme) {
+            assertEquals(title.getText(), "Simbirsoft theme");
+            if (title.getText().equals("Simbirsoft theme"))
+                n++;
+        }
+        if (n < 1) {
+            System.out.println("Писем по теме Simbirsoft theme нет");
+        } else {
+            System.out.println("Найдено " + n + " писем");
+
+        }
+    }
+
+    @Description("Проверка отображения нового письма")
+    @Test
+    public void checkAddingNewMessage() {
+        int count = 0;
+        mailPage.switchNewPage(1);
+        for (WebElement title : mailPage.messageTheme) {
+            if (title.getText().equals("Simbirsoft theme"))
+                n++;
+        }
+        mailPage.newMessage();
+        mailPage.addMailAddress("testovumar@yandex.ru", "Simbirsoft theme");
+        mailPage.sendMessage();
+        mailPage.modalTitle().isDisplayed();
+        driver.navigate().refresh();
+        for (WebElement title : mailPage.messageTheme) {
             if (title.getText().equals("Simbirsoft theme"))
                 count++;
         }
-        System.out.println("Писем по теме найдено " + count);
+        System.out.println("Первый счетчик " + n);
+        System.out.println("Второй счетчик " + count);
+        assert (count > n);
+
     }
-
-
 
 
     @AfterAll
