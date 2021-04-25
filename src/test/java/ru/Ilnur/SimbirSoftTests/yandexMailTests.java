1a6c4fce195e273ebb9c91d0d4f446c9fd5955b3
Umar222 <rabotatest1992@mail.ru>
2021-04-25 12:06:24 +0300

    1) Добавил экземпляр класса MailAuthPage в метод setUp() 2) Метод switchNewPage вернул из класса MailPage 3) Поменял метод проверки отображения текста на assertEquals(mailPage.modalTitle().getText(), "Письмо отправлено"); 4) Добавил отдельный метод пересчитывания писем на почте mailCount, добавил его в класс MailPage 5) Поменял метод сравнения количества писем на jUnit assert, assertTrue(count > n);

--- src/test/java/ru/Ilnur/SimbirSoftTests/yandexMailTests.java
+++ src/test/java/ru/Ilnur/SimbirSoftTests/yandexMailTests.java
@@ -5,70 +5,59 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import ru.Ilnur.Pages.MailAuthPage;
 import ru.Ilnur.Pages.MailPage;
 
+import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class YandexMailTests {
+public class YandexMailTests{
 
     public static WebDriver driver = new ChromeDriver();
+    static MailAuthPage mailAuthPage;
     static MailPage mailPage;
     int n;
 
+    public void switchNewPage(int pageNumber) {
+        ArrayList<String> tabs2 = new ArrayList<String>(driver.getWindowHandles());
+        driver.switchTo().window(tabs2.get(pageNumber));
+    }
+
     @BeforeAll
     public static void setUp() {
+        mailAuthPage = new MailAuthPage(driver);
         mailPage = new MailPage(driver);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://yandex.ru/");
         mailPage.clickEnter();
-        mailPage.openMail("testovumar@yandex.ru", "qwertY116");
+        mailAuthPage.authMail("testovumar@yandex.ru", "qwertY116");
         mailPage.openMailWindow();
     }
 
 
-    @Description("Проверка отображения писем на тему Simbirsoft theme")
-    @Test
-    public void checkEnterMessage() {
-        mailPage.switchNewPage(1);
-        for (WebElement title : mailPage.messageTheme) {
-            assertEquals(title.getText(), "Simbirsoft theme");
-            if (title.getText().equals("Simbirsoft theme"))
-                n++;
-        }
-        if (n < 1) {
-            System.out.println("Писем по теме Simbirsoft theme нет");
-        } else {
-            System.out.println("Найдено " + n + " писем");
-
-        }
-    }
-
     @Description("Проверка отображения нового письма")
     @Test
     public void checkAddingNewMessage() {
-        int count = 0;
-        mailPage.switchNewPage(1);
-        for (WebElement title : mailPage.messageTheme) {
-            if (title.getText().equals("Simbirsoft theme"))
-                n++;
-        }
+
+        switchNewPage(1);
+        int n = mailPage.mailCount("Simbirsoft theme");
+        System.out.println("Найдено " + n + " писем\\ьма");
+
         mailPage.newMessage();
         mailPage.addMailAddress("testovumar@yandex.ru", "Simbirsoft theme");
         mailPage.sendMessage();
-        mailPage.modalTitle().isDisplayed();
+        assertEquals(mailPage.modalTitle().getText(), "Письмо отправлено");
+
         driver.navigate().refresh();
-        for (WebElement title : mailPage.messageTheme) {
-            if (title.getText().equals("Simbirsoft theme"))
-                count++;
-        }
-        System.out.println("Первый счетчик " + n);
-        System.out.println("Второй счетчик " + count);
-        assert (count > n);
+
+        int count = mailPage.mailCount("Simbirsoft theme");
+
+        assertTrue(count > n);
 
     }
 
