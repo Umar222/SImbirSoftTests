5b6a411c11a24d76ef76a0af608a28d6836eeb22
Umar222 <rabotatest1992@mail.ru>
2021-04-29 20:05:11 +0300

    Переименовал метод addMailAddress на inputMailAddress

--- src/test/java/ru/Ilnur/SimbirSoftTests/yandexMailTests.java
+++ src/test/java/ru/Ilnur/SimbirSoftTests/yandexMailTests.java
@@ -6,28 +6,26 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import ru.Ilnur.Pages.General;
+import ru.Ilnur.Pages.BasePage;
 import ru.Ilnur.Pages.MailAuthPage;
 import ru.Ilnur.Pages.MailPage;
 
-import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class YandexMailTests{
+public class YandexMailTests {
 
     public static WebDriver driver = new ChromeDriver();
     static MailAuthPage mailAuthPage;
     static MailPage mailPage;
-    static General general;
-
+    static BasePage basePage;
 
 
     @BeforeAll
     public static void setUp() {
-        general = new General(driver);
+        basePage = new BasePage(driver);
         mailAuthPage = new MailAuthPage(driver);
         mailPage = new MailPage(driver);
         driver.manage().window().maximize();
@@ -42,16 +40,16 @@ public class YandexMailTests{
     @Description("Проверка отображения нового письма")
     @Test
     public void checkAddingNewMessage() {
-            General.switchNewPage(1);
-            int count1 = mailPage.mailCount("Simbirsoft theme");
-            System.out.println("Найдено " + count1 + " писем\\ьма");
-            mailPage.newMessage();
-            mailPage.addMailAddress("testovumar@yandex.ru", "Simbirsoft theme");
-            mailPage.sendMessage();
-            assertEquals(mailPage.modalTitle().getText(), "Письмо отправлено", "Текст не отобразился");
-            driver.navigate().refresh();
-            int count2 = mailPage.mailCount("Simbirsoft theme");
-            assertTrue(count2 > count1,"Новое письмо не создалось");
+        BasePage.switchNewPage(1);
+        int count1 = mailPage.mailCount("Simbirsoft theme");
+        System.out.println("Найдено " + count1 + " писем\\ьма");
+        mailPage.newMessage();
+        mailPage.inputMailAddress("testovumar@yandex.ru", "Simbirsoft theme");
+        mailPage.sendMessage();
+        assertEquals(mailPage.modalTitle().getText(), "Письмо отправлено", "Текст не отобразился");
+        driver.navigate().refresh();
+        int count2 = mailPage.mailCount("Simbirsoft theme");
+        assertTrue(count2 > count1, "Новое письмо не создалось");
     }
 
     @AfterAll
