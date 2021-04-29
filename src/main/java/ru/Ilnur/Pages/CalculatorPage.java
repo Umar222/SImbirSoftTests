cf40dd9da6df85d18034476d91acf788398ee8c8
Umar222 <rabotatest1992@mail.ru>
2021-04-29 20:32:03 +0300

    Добавил отступы между локаторами

--- src/main/java/ru/Ilnur/Pages/CalculatorPage.java
+++ src/main/java/ru/Ilnur/Pages/CalculatorPage.java
@@ -10,36 +10,52 @@ public class CalculatorPage {
 
     @FindBy(css = "td:nth-child(1) > .A2W7l > .MEdqYd")
     private WebElement leftParenthesis;
+
     @FindBy(css = "tr:nth-child(4) > td:nth-child(1) > .A2W7l > .XRsWPe")
     private WebElement buttonOne;
+
     @FindBy(css = "tr:nth-child(5) .mF5fo > .MEdqYd")
     private WebElement plus;
+
     @FindBy(css = "tr:nth-child(4) > td:nth-child(2) > .A2W7l > .XRsWPe")
     private WebElement buttonTwo;
+
     @FindBy(css = "td:nth-child(2) > .A2W7l > .MEdqYd")
     private WebElement RightParenthesis;
+
     @FindBy(css = "tr:nth-child(3) .mF5fo > .XRsWPe")
     private WebElement multiply;
+
     @FindBy(css = "tr:nth-child(4) > td:nth-child(3) > .A2W7l > .XRsWPe")
     private WebElement buttonThree;
+
     @FindBy(css = "tr:nth-child(4) .mF5fo > .XRsWPe")
     private WebElement minus;
+
     @FindBy(css = "tr:nth-child(3) > td:nth-child(1) > .A2W7l > .XRsWPe")
     private WebElement buttonFore;
+
     @FindBy(css = "tr:nth-child(5) .A2W7l > .XRsWPe")
     private WebElement buttonZero;
+
     @FindBy(css = "tr:nth-child(2) .mF5fo > .XRsWPe")
     private WebElement divide;
+
     @FindBy(css = "tr:nth-child(3) > td:nth-child(2) > .A2W7l > .XRsWPe")
     private WebElement buttonFive;
+
     @FindBy(css = "tr:nth-child(3) > td:nth-child(3) > .A2W7l > .XRsWPe")
     private WebElement buttonSix;
+
     @FindBy(xpath = "//div[@jsname = \"aN1RFf\"]")
     private WebElement buttonSin;
+
     @FindBy(xpath = "//span[@jsname = \"ubtiRe\"]")
     private WebElement history;
+
     @FindBy(xpath = "//span[@jsname = \"VssY5c\"]")
     private WebElement numberField;
+
     @FindBy(css = ".UUhRt")
     private WebElement equally;
 
